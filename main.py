579e9acea9a51756131e51adc146993775467a9f
amengdv <aminkafri0082@gmail.com>
2024-08-04 11:10:49 +0800

    feat: characters counter

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def count_characters(text):
+    char_dict = {}
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+
+    return char_dict
+
+
 def count_words(text):
     return len(text.split())
 
@@ -11,7 +22,10 @@ def main():
     path = "books/frankenstein.txt"
     file_contents = get_book_content(path)
     word_count = count_words(file_contents)
+    char_count = count_characters(file_contents)
     print(f"There are {word_count} in this book")
+    print("Character report:")
+    print(char_count)
 
 
 main()
