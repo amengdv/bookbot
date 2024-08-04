0dab70f6a1a48cb68ad774850c9ddb17da404344
amengdv <aminkafri0082@gmail.com>
2024-08-04 11:49:49 +0800

    feat: add report and reformat func

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def sort_on(dict):
+    keys = list(dict)
+    return dict[keys[0]]
+
+
+def reformat_char_dict(char_dict):
+    char_list = []
+    for k in char_dict:
+        if k.isalpha():
+            ch_sep_dict = {k: char_dict[k]}
+            char_list.append(ch_sep_dict)
+
+    char_list.sort(key=sort_on, reverse=True)
+    return char_list
+
+
 def count_characters(text):
     char_dict = {}
     for char in text.lower():
@@ -23,9 +39,14 @@ def main():
     file_contents = get_book_content(path)
     word_count = count_words(file_contents)
     char_count = count_characters(file_contents)
-    print(f"There are {word_count} in this book")
-    print("Character report:")
-    print(char_count)
+    reformatted = reformat_char_dict(char_count)
+    print("----------- REPORT FOR THIS BOOK ------------\n")
+    print(f"There are {word_count} in this book\n")
+    for d in reformatted:
+        for key in d:
+            print(f"There are {d[key]} {key} in this book")
+
+    print("\nREPORT FOR THIS BOOK ----------- END REPORT ------------\n")
 
 
 main()
